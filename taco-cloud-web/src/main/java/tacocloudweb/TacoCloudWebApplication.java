2cedf36f34984b46320682247f11f94b6b4c6a8f
Prashant <prashantbhat94@gmail.com>
2022-04-10 00:28:30 +0530

    first commit

---
+++ taco-cloud-web/src/main/java/tacocloudweb/TacoCloudWebApplication.java
@@ -0,0 +1,13 @@
+package tacocloudweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudWebApplication.class, args);
+	}
+
+}
